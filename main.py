6e141719d1d4d1abaca881aa3f979cb0a11e46d0
Your Name <you@example.com>
2025-05-04 20:47:51 +0200

    Refactor main.py

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import pygame
 import sys
 
 from core.scene_manager import SceneManager
-from core.config import SCREEN_HEIGHT, SCREEN_WIDTH
+from core.config import SCREEN_WIDTH, SCREEN_HEIGHT
 from core.run_manager import run_manager
 from scene.gameover_scene import GameOverScene
 from data.items_loader import get_all_items
 
 def main():
+    """
+    Point d'entrée principal du jeu.
+    Initialise Pygame, charge les objets, configure la scène initiale et lance la boucle principale.
+    """
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Pokémon Eclipse")
@@ -15,11 +19,11 @@ def main():
     clock = pygame.time.Clock()
     scene_manager = SceneManager()
 
-    items = get_all_items()
-    for item in items:
+    # Charger tous les objets dans l'inventaire
+    for item in get_all_items():
         run_manager.add_item(item)
 
-
+    # Lancer la première scène (Game Over ici pour test/démo)
     scene_manager.change_scene(GameOverScene())
 
     running = True
